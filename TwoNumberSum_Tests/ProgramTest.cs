58a07120c70d58ef56b3fd68a106333c7e3070e9
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-07-06 09:40:14 +0200

    .

--- TwoNumberSum_Tests/ProgramTest.cs
+++ TwoNumberSum_Tests/ProgramTest.cs
@@ -10,7 +10,7 @@ namespace TwoNumberSum_Tests
         public void Test1()
         {
             int[] output = Program.TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
-           // Assert.True(output.Length == 2);
+            Assert.True(output.Length == 2);
             Assert.True(Array.Exists(output, e => e == -1));
             Assert.True(Array.Exists(output, e => e == 11));
         }
