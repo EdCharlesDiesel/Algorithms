025205e13c9505d45ab13bebf8b5835e42b848f5
Khotso Mokhethi <Mokhetkc@hotmail.com>
2022-02-05 23:21:50 +0200

    .

---
+++ BranchSums/Program.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BranchSums
+//{
+//    using System.Collections.Generic;
+//public class Program {
+// public class BinaryTree {
+// public int value;
+// public BinaryTree left;
+// public BinaryTree right;
+// public BinaryTree(int value) {
+// this.value = value;
+// this.left = null;
+// this.right = null;
+// }
+// }
+// // O(n) time | O(n) space - where n is the number of nodes in the Bi
+// public static List<int> BranchSums(BinaryTree root) {
+// List<int> sums = new List<int>();
+// calculateBranchSums(root, 0, sums);
+// return sums;
+// }
+// public static void calculateBranchSums(BinaryTree node, int runningS
+// if (node == null) return;
+// int newRunningSum = runningSum + node.value;
+// if (node.left == null && node.right == null) {
+// sums.Add(newRunningSum);
+// return;
+// }
+//1
+//2
+//3
